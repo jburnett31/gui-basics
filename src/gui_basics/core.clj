bc0efd0a5fea8688b6f452ab8c8b7a3c1ce70a43
jburnett31 <jeffrey.burnett31@gmail.com>
2012-08-28 14:20:02 -0500

    first

---
+++ src/gui_basics/core.clj
@@ -0,0 +1,6 @@
+(ns gui-basics.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/gui_basics/core_test.clj
@@ -0,0 +1,7 @@
+(ns gui-basics.core-test
+  (:use clojure.test
+        gui-basics.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
